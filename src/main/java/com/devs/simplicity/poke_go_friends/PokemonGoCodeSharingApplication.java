9c8d1522b7c22f59f98562684a55170cf2f12f91
Michael Farrelly <simplicitydevs@gmail.com>
2025-06-17 04:30:07 +0900

    Initial commit

---
+++ src/main/java/com/devs/simplicity/poke_go_friends/PokemonGoCodeSharingApplication.java
@@ -0,0 +1,13 @@
+package com.devs.simplicity.poke_go_friends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonGoCodeSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonGoCodeSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devs/simplicity/poke_go_friends/PokemonGoCodeSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devs.simplicity.poke_go_friends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonGoCodeSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
