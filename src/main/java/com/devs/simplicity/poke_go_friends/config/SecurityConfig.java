21ac9d03b1dbdb0915ca0dc372feb8b2e655db81
Michael Farrelly <simplicitydevs@gmail.com>
2025-07-16 03:08:44 +0900

    Fixed cors security configs

--- src/main/java/com/devs/simplicity/poke_go_friends/config/SecurityConfig.java
+++ src/main/java/com/devs/simplicity/poke_go_friends/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.devs.simplicity.poke_go_friends.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,8 +18,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .authorizeHttpRequests(auth -> auth
+        http.cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
                 .anyRequest().permitAll()
             );
         return http.build();
